7106cc7fd88228948069ed0dce6a0f75f075c2f9
gottu52 <89571642+gottu52@users.noreply.github.com>
2021-10-06 21:52:06 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { PrimaryButton } from "./components/attoms/buttons/PrimaryButton";
+import { SecondaryButton } from "./components/attoms/buttons/SecondaryButton";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>sample</PrimaryButton>
+      <SecondaryButton>sample</SecondaryButton>
     </div>
   );
 }
--- src/components/attoms/buttons/PrimaryButton.jsx
+++ src/components/attoms/buttons/PrimaryButton.jsx
@@ -1,19 +1,11 @@
 import styled from "styled-components";
+import { BaseButton } from "./BaseButton";
 
 export const PrimaryButton = (props) => {
   const { children } = props;
   return <SButton>{children}</SButton>;
 };
 
-const SButton = styled.button`
+const SButton = styled(BaseButton)`
   background-color: #40514b;
-  color: #fff;
-  padding: 6px 24px;
-  border: none;
-  outline: none;
-  border-radius: 999px;
-  &:hover {
-    cursor: pointer;
-    opacity: 0.8;
-  }
 `;
